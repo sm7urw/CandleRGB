04391f5217caf9cdcb0616ebe98268f04ef042f5
sm7urw <henrik@korslind.com>
2025-01-26 13:55:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
-# Flickering RGB-led Python-script for Raspberry PI Pico W
-# Author: Henrik Korslind (henrik@korslind.com)
-
 from machine import Pin, PWM
 import time
 import urandom
+
+# Konstanter
+PWM_FREQ = 1000
+BASE_RED = 1.0
+BASE_GREEN = 0.5
+BASE_BLUE = 0.0
+FLICKER_GREEN_MIN = 0.4
+FLICKER_GREEN_MAX = 0.6
+FLICKER_DELAY_MIN = 0.04
+FLICKER_DELAY_MAX = 0.12
 
 # Ange pinnummer för RGB LED
 red_pin = PWM(Pin(0))
 green_pin = PWM(Pin(1))
 blue_pin = PWM(Pin(2))
 
-# Konfigurera varje pin som PWM
-red_pin.freq(1000)
-green_pin.freq(1000)
-blue_pin.freq(1000)
+# Initiera PWM
+def init_pwm():
+    red_pin.freq(PWM_FREQ)
+    green_pin.freq(PWM_FREQ)
+    blue_pin.freq(PWM_FREQ)
 
+# Sätt RGB-färger
 def set_rgb(red, green, blue):
     red_pin.duty_u16(int(red * 65535))
     green_pin.duty_u16(int(green * 65535))
     blue_pin.duty_u16(int(blue * 65535))
 
-def flicker():
-    # Sätt en grundläggande färg för "ljuset"
-    set_rgb(1.0, 0.5, 0.0) # Varmt gult ljus
-    time.sleep(urandom.uniform(0.07, 0.12)) # Vänta slumpmässigt mellan 70 och 120 ms
+# Mjuk fade-out när programmet avbryts
+def fade_out():
+    for i in range(100, -1, -1):
+        factor = i / 100
+        set_rgb(BASE_RED * factor, BASE_GREEN * factor, BASE_BLUE * factor)
+        time.sleep(0.01)
 
-    # Slumpmässigt justera färgen för att simulera flimring
-    set_rgb(1.0, urandom.uniform(0.4, 0.6), 0.0)
-    time.sleep(urandom.uniform(0.04, 0.08)) # Vänta slumpmässigt mellan 40 och 80 ms
+# Flimra LED
+def flicker():
+    # Sätt grundfärgen
+    set_rgb(BASE_RED, BASE_GREEN, BASE_BLUE)
+    time.sleep(urandom.uniform(FLICKER_DELAY_MIN, FLICKER_DELAY_MAX))
+
+    # Justera färgen för flimrande effekt
+    flicker_green = urandom.uniform(FLICKER_GREEN_MIN, FLICKER_GREEN_MAX)
+    set_rgb(BASE_RED, flicker_green, BASE_BLUE)
+    time.sleep(urandom.uniform(FLICKER_DELAY_MIN, FLICKER_DELAY_MAX))
+
+# Kör programmet
+init_pwm()
 
 try:
     while True:
         flicker()
 except KeyboardInterrupt:
-    set_rgb(0, 0, 0) # Stäng av LED när skriptet avslutas
+    fade_out()  # Mjuk fade-out
+    set_rgb(0, 0, 0)  # Släck helt efter fade-out
